ecafa1b77259fbe52be85d2b549f5141409b285f
vineetmhapsekar <vineetmhapsekar@yahoo.co.in>
2018-03-17 00:56:00 +0800

    Build directory deletion

--- build/_CPack_Packages/Linux/TGZ/CPPCMakeProject-1.0.0/1.0.0/Debug/include/mylib/sum.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SUM_HPP
-#define SUM_HPP
-
-#include "version.hpp"
-#include <iostream>
-
-int Sum(int a, int b);
-
-#endif // SUM_HPP
--- build/test/mylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinit/workspace/c++/CPPCMakeProject/test/mylib
-# Build directory: /home/vinit/workspace/c++/CPPCMakeProject/build/test/mylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(mylib_complete_test "mylibunittest")
