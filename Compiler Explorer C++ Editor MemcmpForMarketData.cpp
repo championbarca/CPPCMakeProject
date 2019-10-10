21c2fb74c66e5b12f365f95f30dcc8f3c3b3d949
championbarca <vineetmhapsekar@yahoo.co.in>
2019-10-10 19:27:39 +0800

    Interesting behaviour with memcmp between compilers ( < gcc 7 vs > gcc 7 )

---
+++ Compiler Explorer C++ Editor MemcmpForMarketData.cpp
@@ -0,0 +1,23 @@
+﻿// Type your code here, or load an example.
+#include <string.h>
+
+int square(char* num) {
+    if (memcmp(num, "B6", 2) == 0)
+        return 1;
+    else
+        return 0;
+}
+
+int square1(char* num) {
+    if (num[0] == 'B' && num[1] == '6')
+        return 1;
+    else
+        return 0;
+}
+
+int square2(char* num) {
+    if (*(reinterpret_cast<int*>(num)) == 13890)
+        return 1;
+    else
+        return 0;
+}
